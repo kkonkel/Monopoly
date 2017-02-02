7b37745e3011ae0250583bf6c22f793f7f290d61
Kaitlin Konkel <kaitlin.konkel@mullenmail.com>
2017-02-02 11:08:09 -0700

    spaces

--- Monopoly/src/spaces.java
+++ Monopoly/src/spaces.java
@@ -1,8 +1,53 @@
 
-abstract class spaces
+public class spaces
 	{
-protected String name;
-protected int place; // spaces 1-40
-protected boolean isCard;
-abstract void preformsAction();
-	}
+		private int price;
+		private String name;
+		private int location;
+		private String color;
+		public spaces(int p, String n, int l, String c)
+			{
+			price = p;
+			name = n;
+			location = l;
+			}	
+		public String getColor()
+			{
+				return color;
+			}
+		public void setColor(String color)
+			{
+				this.color = color;
+			}
+		//price
+		public int getPrice()
+			{
+			return price;
+			}
+		public void setPrice(int price)
+			{
+			this.price = price;
+			}
+		//location
+		public int getLocation()
+			{
+			return location;
+			}
+		public void setLocation(int location)
+			{
+			this.location = location;
+			}
+		//name
+		public String getName()
+			{
+			return name;
+			}
+
+		public void setName(String name)
+			{
+			this.name = name;
+			}
+
+
+		}
+	
